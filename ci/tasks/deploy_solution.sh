7085c4bb61209598d3031d085e151bf5960fc163
Mateusz Matocha <mateusz.matocha@grapeup.com>
2019-03-29 10:07:41 +0100

    automatic installation

--- ci/tasks/deploy_solution.sh
+++ ci/tasks/deploy_solution.sh
@@ -5,7 +5,7 @@ DEPLOYMENT_JSON=source-code/config/deployment.amd64.json
 
 az login --service-principal -u $USERNAME -p $PASSWORD --tenant $AZ_AD
 wget https://github.com/Azure/azure-iot-cli-extension/releases/download/v0.7.0/azure_cli_iot_ext-0.7.0-py2.py3-none-any.whl
-az extension add --source ./azure_cli_iot_ext-0.7.0-py2.py3-none-any.whl
+az extension add --source ./azure_cli_iot_ext-0.7.0-py2.py3-none-any.whl --yes
 VERSION=$(grep -m 1 "<version>" $MODULE_PATH/pom.xml | cut -d '>' -f 2 | cut -d '<' -f 1)
 sed -i 's/#AZURE_CR_USERNAME/'$USERNAME'/g' $DEPLOYMENT_JSON
 sed -i 's/#AZURE_CR_PASSWORD/'$PASSWORD'/g' $DEPLOYMENT_JSON
