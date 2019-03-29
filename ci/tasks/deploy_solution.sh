b1933ad22bb7a5c7234291f8d40c244ecd9267ef
Mateusz Matocha <mateusz.matocha@grapeup.com>
2019-03-29 08:49:35 +0100

    script updates

--- ci/tasks/deploy_solution.sh
+++ ci/tasks/deploy_solution.sh
@@ -5,7 +5,7 @@ DEPLOYMENT_JSON=source-code/config/deployment.amd64.json
 
 az login --service-principal -u $USERNAME -p $PASSWORD --tenant $AZ_AD
 az extension add --name azure-cli-iot-ext
-VERSION=$(grep -m 1 "<version>" $MODULE_PATH/pom.xml | cut -d '>' -f 2 | cut -d '<' -f 1)
+export VERSION=$(grep -m 1 "<version>" $MODULE_PATH/pom.xml | cut -d '>' -f 2 | cut -d '<' -f 1)
 sed -i 's/#AZURE_CR_USERNAME/$USERNAME/g' $DEPLOYMENT_JSON
 sed -i 's/#AZURE_CR_PASSWORD/$PASSWORD/g' $DEPLOYMENT_JSON
 sed -i 's/#VERSION/$VERSION/g' $DEPLOYMENT_JSON
