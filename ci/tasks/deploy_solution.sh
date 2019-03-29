06ad269655c2932a37e3b77f04d566c5b8ab6a9d
Mateusz Matocha <mateusz.matocha@grapeup.com>
2019-03-29 08:57:23 +0100

    fixed script

--- ci/tasks/deploy_solution.sh
+++ ci/tasks/deploy_solution.sh
@@ -5,10 +5,10 @@ DEPLOYMENT_JSON=source-code/config/deployment.amd64.json
 
 az login --service-principal -u $USERNAME -p $PASSWORD --tenant $AZ_AD
 az extension add --name azure-cli-iot-ext
-export VERSION=$(grep -m 1 "<version>" $MODULE_PATH/pom.xml | cut -d '>' -f 2 | cut -d '<' -f 1)
+VERSION=$(grep -m 1 "<version>" $MODULE_PATH/pom.xml | cut -d '>' -f 2 | cut -d '<' -f 1)
 sed -i 's/#AZURE_CR_USERNAME/$USERNAME/g' $DEPLOYMENT_JSON
 sed -i 's/#AZURE_CR_PASSWORD/$PASSWORD/g' $DEPLOYMENT_JSON
-sed -i 's/#VERSION/$VERSION/g' $DEPLOYMENT_JSON
+sed -i 's/#VERSION/'$VERSION'/g' $DEPLOYMENT_JSON
 
 
 az iot edge deployment create -d test_${VERSION//./_} -n $IOT_HUB_NAME --content $DEPLOYMENT_JSON --target-condition "deviceId='"$DEVICE_ID"'"
